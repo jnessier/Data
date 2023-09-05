6247eaa9049b3e4574cd2630d1590d1cefad5b0a
Jonathan Nessier <jonathan.nessier@neoflow.ch>
2023-09-05 22:02:21 +0200

    Fixed typos and renamed lib

--- src/Data.php
+++ src/Data.php
@@ -13,11 +13,14 @@ class Data implements DataInterface
      * Constructor.
      *
      * @param array|null $values Array with key/value pairs
+     * @param bool $recursive Set FALSE to disable recursive array handling
      */
-    public function __construct(array $values = null)
+    public function __construct(array $values = null, bool $recursive = true)
     {
         if (!is_null($values)) {
             $this->setAll($values);
         }
+
+        $this->recursive = $recursive;
     }
 }
